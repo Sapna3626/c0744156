2e46f38985145787f5b6f42bdcd1b1a873cbca74
visualstudio <visualstudio@230-PC11>
2019-02-11 18:20:52 -0500

    //final

--- c0744156/js/main.js
+++ c0744156/js/main.js
@@ -1,1 +1,19 @@
 ﻿// Your code here!
+function myfunction(a, b) {
+
+    return a + b;  //the function return the product of p1 and p2
+}
+
+
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "blue", "green", "pink", "purple"];
+    marker = pens;
+    console.log(marker);
+    console.log(marker[0]);
+    for (i = 0; i < marker.length; i++) {
+        alert(marker[i]);
+    }
+    alert("length of arrays is :" + marker.length);
+}
